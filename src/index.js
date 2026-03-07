aea09a929086e31a84e7cdc8309fca7acce2496d
farhattyagi8-del <farhattyagi8@gmail.com>
2026-03-07 12:43:26 +0530

    add all changes

---
+++ src/index.js
@@ -0,0 +1,53 @@
+ import mongoose from "mongoose";
+ import { DB_NAME } from "./constant.js";
+
+
+
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: '.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+//   const app = express();
+// (async () => {
+//     try {
+//           await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, )
+//           app.on("error", (error) => {            console.error("Error connecting to MongoDB:", error);
+//           });
+//           console.log("Connected to MongoDB successfully");
+
+//        app.listen(process.env.PORT, () => {
+//         console.log (`app is listen on port ${process.env.PORT}`);
+//        })
+
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);  
+//         throw error; // Rethrow the error to be handled by the caller 
+//     }
+// }) ();
